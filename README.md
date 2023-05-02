aff780774c33ca935cb3292f10079a265b6d547d
Ivan Sim <ivan@validus.sg>
2023-05-02 10:59:12 +0800

    first common

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # airflow-spark-project
+
+## _Instruction_
+
+1. Open the parent directory of the project in VS code or your preferred code editors and open the **terminal**.
+2. Execute **. start.sh** in the terminal to setup the docker environments.
+3. Setup email alart
+   i. Setup app passwords in https://security.google.com/settings/security/apppasswords
+   ii. Setup smtp under **airflow.cfg** as below (The rests remind the same)
+       smtp_host = smtp.gmail.com
+       smtp_user = {your email}
+       smtp_password = {password generated from app password}
+       smtp_port = 587
+       smtp_mail_from = {your email}
+4. Open web browser and go to **http://localhost:8080/**
+   user: airflow
+   password: airflow
+5. Setup spark connection
+   i. Click on Admin on the top bar
+   ii. Click on Connection and add a new connection name **spark_conn**
+   iii. Change the conn Type to **Spark**
+   iv. Input Host as **spark://spark-master**
+   v. Input Port as **7077**
+   vi. Save it
+5. Switch on the DAG **process_csv** and click on it.
+6. Click the play buttom on the top right for manual triggers (else it will be shceduled at 12am UTC daily)
+7. Close the project by executing **. stop.sh** in terminal
+
+**Note:** Assuming that you have already set up a basic development environment and docker desktop on your workstation.
---
+++ mnt/airflow/dags/data_pipeline.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from pyspark.sql import SparkSession
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime, timedelta
+from scripts.setup_logger import setup_logging
+
+logger = setup_logging()
+
+# Define callback functions
+def on_success_task(dict):
+    """Log a success message when a task succeeds."""
+    logger.info('Task succeeded: %s', dict)
+
+def on_failure_task(dict):
+    """Log an error message when a task fails."""
+    logger.error('Task failed: %s', dict)
+
+default_args = {
+    'owner': 'Airflow',
+    'retries':3,
+    'retry_delay': timedelta(seconds=60),
+    'emails': ['ivansim0831@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'on_failure_callback': on_failure_task,
+    'on_success_callback': on_success_task,
+    'execution_time': timedelta(seconds=60)
+}
+
+
+
+with DAG("process_csv", start_date=datetime(2023, 5, 1), schedule_interval=timedelta(days=1), default_args=default_args, catchup=False) as dag:
+
+    process_csv = SparkSubmitOperator(
+        task_id="process_csv",
+        application="/opt/airflow/dags/scripts/process_csv.py",
+        conn_id="spark_conn",
+        application_args=["applications_dataset_1.csv", "applications_dataset_2.csv"]
+,
+        verbose= False
+    )
+
+    process_csv
